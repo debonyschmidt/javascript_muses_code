aa080c6efcaf029b5ea1aed31ffff29e3c67de52
Debony SCHMIDT <debony_13_@hotmail.com>
2019-08-25 12:51:00 +1000

    Update main.js

--- js-xmas-edition-master/js/main.js
+++ js-xmas-edition-master/js/main.js
@@ -1,29 +1,75 @@
-const city = document.letterToSanta.city.value;
 const behaviour = document.letterToSanta.goodVSnaughty.value;
-const description = document.letterToSanta.description.value;
 
-function nameValidationError (value) {
+function nameValidationError(value) {
 
     if (value === "") {
         return ("Name is required");
 
     } else if (value.length < 2) {
-        return ("This field sould be longer than 1 character.");
+        return ("The name sould be longer than 1 character.");
 
     } else if (value.length > 250) {
-        return("This field cannot be longer than 250 characters.");
+        return("The name cannot be longer than 250 characters.");
 
     } else if (/^[A-z]+$/.test(value) === false) {
         return("Please, enter only letters in the name field.");    
     }
-        return "";
+        
 }
 
+function cityValidationError(value) {
+    
+    if (value == "") {
+        return ("City is required");
+        
+    }
+}
+
+function descriptionValidationError(value) {
+
+    if (value == ""){
+        return ("Description is required");
+
+    } else if (value.length < 2) {
+        return ("The description sould be longer than 1 character.");
+
+    } else if (value.length > 250) {
+        return("The description cannot be longer than 250 characters.");
+
+    } else if (/^[A-z0-9]+$/.test(value) === false) {
+        return("Please, enter only letters in the name field.");    
+    }
+}
 function validateForm(event) {
     const name = document.letterToSanta.name.value;
+    const city = document.letterToSanta.city.value;
+    const description = document.letterToSanta.description.value;
+    
+    const errors = {
+        name: nameValidationError(name),
+        city: cityValidationError(city),
+        description: descriptionValidationError(description),
+    };
+
     nameValidationError (name);
-    console.log(nameValidationError(name));
+    cityValidationError(city);
+    descriptionValidationError(description);
+
+    handleErrors(errors);
     event.preventDefault();
 }
+
+function handleErrors(errors) {
+    let errorsCount = 0;
+
+    Object.keys(errors)
+    .forEach(function (key) {
+        console.log(errors[key]);
+        errorsCount = errorsCount + errors[key].length;
+    });
+
+}
+
+
 
 
